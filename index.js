a2cfae1b9d0ad4718fc40e23e26b4cf26259f871
Joswag Sandoval <sergiojr100@outlook.com>
2021-11-21 21:36:17 -0600

    added more functionality to the game and added difficulty levels as well as i could not find out how to gradually decrease time interval in setInterval.

--- index.js
+++ index.js
@@ -2,44 +2,81 @@
 var playing = false
 const btn1 = document.querySelector(".button1")
 const btn2 = document.querySelector('.button2')
-const btn3 = document.querySelector('.button3')
+
+const easy = document.querySelector('.easy')
+const medium = document.querySelector('.medium')
+const hard = document.querySelector('.hard')
 
 console.log(playing)
 
+
+let easyTime = 600
+let mediumTime = 400
+let hardTime = 320
+
+const interval = (x) => {
+    setInterval(position, x)
+}
+
+const easyDifficulty = () => {
+    time = easyTime
+    easy.classList.add('hidden')
+    interval(time)
+}
+const mediumDifficulty = () => {
+    time = mediumTime
+    medium.classList.add('hidden')
+    interval(time)
+}
+const hardDifficulty = () => {
+    time = hardTime
+    hard.classList.add('hidden')
+    interval(time)
+}
+
 const startGame = () => {
     btn1.classList.add('hidden')
-    btn2.classList.add('hidden')
-    playing = true
-    position()
+    btn2.classList.remove('hidden')
 
-    
+    easy.classList.remove('hidden')
+    medium.classList.remove('hidden')
+    hard.classList.remove('hidden')
+
+    easy.addEventListener('click', easyDifficulty)
+    medium.addEventListener('click', mediumDifficulty)
+    hard.addEventListener('click', hardDifficulty)
+        
+    playing = true
+    // interval(time)
+
+    // playing = false
+
 }
 
 btn1.addEventListener('click',startGame)
 
-const controlScreen = () => {
-    btn1.classList.add('hidden')
-    btn2.classList.add('hidden')
-}
-
-btn2.addEventListener('click', controlScreen)
-
 const backButton = () => {
     btn1.classList.remove('hidden')
-    btn2.classList.remove('hidden')
+    btn2.classList.add('hidden')
     playing = false
 }
 
-btn3.addEventListener('click',backButton)
+btn2.addEventListener('click',backButton)
 
 //Coding for game 
+var onScreen = 0
 var lvlClicks = 0
-lvlPassed = false
-level = 1
+var lvlPassed = false
+var level = 1
 const container = document.querySelector('.container')
 
 
 const position = () =>{
+    const clicked = () =>{
+        square.classList.remove('square')
+        lvlClicks++
+        onScreen--
+    }
     const square = document.createElement('div')
     square.classList.add('square')
 
@@ -49,9 +86,25 @@ const position = () =>{
     square.style.top = yPos+"px"
     square.style.left = xPos+"px"
     container.appendChild(square)
-    console.log(yPos)
-    console.log(xPos)
+    // console.log(yPos)
+    // console.log(xPos)
+
+    onScreen++
+
+    if (onScreen === 20){
+        console.log(onScreen)
+        alert('You lost!')
+        
+    }
+
+    square.addEventListener('click', clicked)
+    console.log('squares clicked: '+lvlClicks)
+    console.log("on screen "+onScreen)
+    console.log('playing '+playing)
 }
+
+
+
 
 
 
